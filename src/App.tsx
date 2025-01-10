dd960318474abf5caf9f5cdea46193cc2aa7c6e7
Akhigbepeace <peaceakhigbe18@gmail.com>
2025-01-10 19:11:48 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Welcome to Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
